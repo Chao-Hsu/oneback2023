18472c4fc73d096105f7784cb0e24ffdcc3935b7
ChaoHsu.Chen <chaohsu.chen@onelab.tw>
2023-12-05 17:44:29 +0800

    init

---
+++ OneBackComboTrainingWeb/TennisGame.cs
@@ -0,0 +1,65 @@
+﻿namespace OneBackComboTrainingWeb
+{
+    public class TennisGame
+    {
+        private string _firstPlayer;
+        private string _secondPlayer;
+        private int _firstPlayerScore;
+        private int _secondPlayerScore;
+        private Dictionary<int, string> _scoreDictionary = new()
+        {
+            { 0, "love" },
+            { 1, "fifteen" },
+            { 2, "thirty" },
+            { 3, "forty" },
+        };
+
+        public TennisGame(string firstPlayer, string secondPlayer)
+        {
+            _firstPlayer = firstPlayer;
+            _secondPlayer = secondPlayer;
+        }
+        public string Score()
+        {
+            if (_firstPlayerScore >= 3 && _secondPlayerScore >= 3)
+            {
+                if (_firstPlayerScore == _secondPlayerScore)
+                    return "deuce";
+                if (Math.Abs(_firstPlayerScore - _secondPlayerScore) == 1)
+                    return _firstPlayerScore > _secondPlayerScore
+                        ? $"{_firstPlayer} adv"
+                        : $"{_secondPlayer} adv";
+                if (Math.Abs(_firstPlayerScore - _secondPlayerScore) == 2)
+                    return _firstPlayerScore > _secondPlayerScore
+                        ? $"{_firstPlayer} win"
+                        : $"{_secondPlayer} win";
+                return "Error";
+            }
+
+            if (_firstPlayerScore == _secondPlayerScore)
+            {
+                return $"{_scoreDictionary[_firstPlayerScore]} all";
+            }
+
+            if (_firstPlayerScore == 4)
+            {
+                return $"{_firstPlayer} win";
+            }
+            if (_secondPlayerScore == 4)
+            {
+                return $"{_secondPlayer} win";
+            }
+
+            return $"{_scoreDictionary[_firstPlayerScore]} {_scoreDictionary[_secondPlayerScore]}";
+        }
+
+        public void AddFirstPlayerScore()
+        {
+            _firstPlayerScore++;
+        }
+        public void AddSecondPlayerScore()
+        {
+            _secondPlayerScore++;
+        }
+    }
+}
---
+++ OneBackTests/TennisGameTest.cs
@@ -0,0 +1,185 @@
+﻿using FluentAssertions;
+using NSubstitute;
+using OneBackComboTrainingWeb;
+
+namespace OneBackTests
+{
+    public class TennisGameTest
+    {
+        private TennisGame _tennisGame;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _tennisGame = new TennisGame("Eva", "Eric");
+        }
+
+        [Test]
+        public void love_all()
+        {
+            ScoreShouldBe("love all");
+        }
+
+        [Test]
+        public void fifteen_love()
+        {
+            _tennisGame.AddFirstPlayerScore();
+            ScoreShouldBe("fifteen love");
+        }
+
+        [Test]
+        public void love_fifteen()
+        {
+            _tennisGame.AddSecondPlayerScore();
+            ScoreShouldBe("love fifteen");
+        }
+
+        [Test]
+        public void fifteen_all()
+        {
+            _tennisGame.AddFirstPlayerScore();
+            _tennisGame.AddSecondPlayerScore();
+            ScoreShouldBe("fifteen all");
+        }
+
+        [Test]
+        public void thirty_love()
+        {
+            _tennisGame.AddFirstPlayerScore();
+            _tennisGame.AddFirstPlayerScore();
+            ScoreShouldBe("thirty love");
+        }
+
+        [Test]
+        public void love_thirty()
+        {
+            _tennisGame.AddSecondPlayerScore();
+            _tennisGame.AddSecondPlayerScore();
+            ScoreShouldBe("love thirty");
+        }
+
+        [Test]
+        public void thirty_all()
+        {
+            _tennisGame.AddFirstPlayerScore();
+            _tennisGame.AddFirstPlayerScore();
+            _tennisGame.AddSecondPlayerScore();
+            _tennisGame.AddSecondPlayerScore();
+            ScoreShouldBe("thirty all");
+        }
+
+        [Test]
+        public void thirty_fifteen()
+        {
+            _tennisGame.AddFirstPlayerScore();
+            _tennisGame.AddFirstPlayerScore();
+            _tennisGame.AddSecondPlayerScore();
+            ScoreShouldBe("thirty fifteen");
+        }
+
+        [Test]
+        public void fifteen_thirty()
+        {
+            _tennisGame.AddFirstPlayerScore();
+            _tennisGame.AddSecondPlayerScore();
+            _tennisGame.AddSecondPlayerScore();
+            ScoreShouldBe("fifteen thirty");
+        }
+
+        [Test]
+        public void deuce_when_3_3()
+        {
+            _tennisGame.AddFirstPlayerScore();
+            _tennisGame.AddFirstPlayerScore();
+            _tennisGame.AddFirstPlayerScore();
+            _tennisGame.AddSecondPlayerScore();
+            _tennisGame.AddSecondPlayerScore();
+            _tennisGame.AddSecondPlayerScore();
+            ScoreShouldBe("deuce");
+        }
+
+        [Test]
+        public void deuce_when_4_4()
+        {
+            _tennisGame.AddFirstPlayerScore();
+            _tennisGame.AddFirstPlayerScore();
+            _tennisGame.AddFirstPlayerScore();
+            _tennisGame.AddFirstPlayerScore();
+            _tennisGame.AddSecondPlayerScore();
+            _tennisGame.AddSecondPlayerScore();
+            _tennisGame.AddSecondPlayerScore();
+            _tennisGame.AddSecondPlayerScore();
+            ScoreShouldBe("deuce");
+        }
+
+        [Test]
+        public void eva_adv_when_4_3()
+        {
+            _tennisGame.AddFirstPlayerScore();
+            _tennisGame.AddFirstPlayerScore();
+            _tennisGame.AddFirstPlayerScore();
+            _tennisGame.AddFirstPlayerScore();
+            _tennisGame.AddSecondPlayerScore();
+            _tennisGame.AddSecondPlayerScore();
+            _tennisGame.AddSecondPlayerScore();
+            ScoreShouldBe("Eva adv");
+        }
+
+        [Test]
+        public void eric_adv_when_3_4()
+        {
+            _tennisGame.AddFirstPlayerScore();
+            _tennisGame.AddFirstPlayerScore();
+            _tennisGame.AddFirstPlayerScore();
+            _tennisGame.AddSecondPlayerScore();
+            _tennisGame.AddSecondPlayerScore();
+            _tennisGame.AddSecondPlayerScore();
+            _tennisGame.AddSecondPlayerScore();
+            ScoreShouldBe("Eric adv");
+        }
+
+        [Test]
+        public void eric_win_when_3_5()
+        {
+            _tennisGame.AddFirstPlayerScore();
+            _tennisGame.AddFirstPlayerScore();
+            _tennisGame.AddFirstPlayerScore();
+            _tennisGame.AddSecondPlayerScore();
+            _tennisGame.AddSecondPlayerScore();
+            _tennisGame.AddSecondPlayerScore();
+            _tennisGame.AddSecondPlayerScore();
+            _tennisGame.AddSecondPlayerScore();
+            ScoreShouldBe("Eric win");
+        }
+
+        [Test]
+        public void eva_win_when_5_3()
+        {
+            _tennisGame.AddFirstPlayerScore();
+            _tennisGame.AddFirstPlayerScore();
+            _tennisGame.AddFirstPlayerScore();
+            _tennisGame.AddFirstPlayerScore();
+            _tennisGame.AddFirstPlayerScore();
+            _tennisGame.AddSecondPlayerScore();
+            _tennisGame.AddSecondPlayerScore();
+            _tennisGame.AddSecondPlayerScore();
+            ScoreShouldBe("Eva win");
+        }
+
+        [Test]
+        public void eva_win_when_4_0()
+        {
+            _tennisGame.AddFirstPlayerScore();
+            _tennisGame.AddFirstPlayerScore();
+            _tennisGame.AddFirstPlayerScore();
+            _tennisGame.AddFirstPlayerScore();
+            ScoreShouldBe("Eva win");
+        }
+
+        private void ScoreShouldBe(string expected)
+        {
+            var score = _tennisGame.Score();
+            score.Should().Be(expected);
+        }
+    }
+}
